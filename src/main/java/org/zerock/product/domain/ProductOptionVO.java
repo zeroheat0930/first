866a9a181d0f403b7f273c12c9dd0e7c5f3ea88f
zeroheat0930 <dongjunkorea@naver.com>
2021-07-13 17:54:07 +0900

    master

--- src/main/java/org/zerock/product/domain/ProductOptionVO.java
+++ src/main/java/org/zerock/product/domain/ProductOptionVO.java
@@ -1,28 +1,13 @@
 package org.zerock.product.domain;
 
-import java.util.Date;
-
 import lombok.Data;
 
 @Data
-public class ProductVO {
-	//등록시하드코딩컬럼
+public class ProductOptionVO {
+	private int productOption_seq;
 	private int product_seq;
-	//업데이트시 수정안되는컬럼
-	private int product_price;
-	private int product_quantity;
-	private int product_seller;
-	//업데이트시 수정되는컬럼(+updatedate하드코딩)
-	private String product_name;
-	private String user_nickname;
-	private String product_filename;
-	private String product_info;
-	private int category_seq;
-	//default가 정해져있는 컬럼
-	private int product_readcnt;
-	private int product_status;
-	private Date product_regdate;
-	private Date product_updatedate;
-	private int product_like;
-
+	private String po_name;
+	private int po_quantity;
+	private int po_price;
+	
 }
--- src/main/webapp/WEB-INF/tags/sale/navbar.tag
+++ src/main/webapp/WEB-INF/tags/sale/navbar.tag
@@ -1,7 +1,6 @@
 <%@ tag language="java" pageEncoding="UTF-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
-<%@ taglib prefix="sec" uri="http://www.springframework.org/security/tags" %>
 
 <nav class="navbar navbar-expand-sm navbar-light bg-light justify-content-center">
    <ul class="navbar-nav">
